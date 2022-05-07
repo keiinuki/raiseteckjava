e2c53fed53f1cd0874f9365f406d90fb8f6c17d5
keiinuki <k.inuki@haguruma.co.jp>
2022-05-08 06:53:41 +0900

    課題１　while文で出力

--- Task1/src/task/HelloWorld.java
+++ Task1/src/task/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-        int i = 0;
+        int i = 1;
         while(i <= 10){
         	System.out.println("Hello World " + i);
         	i = i + 1;
